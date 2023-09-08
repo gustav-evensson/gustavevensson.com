e2fea9816ae79101088b2a58b0a3b9ae6c8d1c98
Gustav Evensson <evenssongustav@gmail.com>
2023-09-08 13:18:40 +0200

    added color api

--- website/src/views/workView.vue
+++ website/src/views/workView.vue
@@ -5,6 +5,7 @@ import projectCard from '../components/projectCard.vue';
 import aerialShotsImg from '../assets/projectImages/aerialshots.jpg';
 import allStoreImg from '../assets/projectImages/allstore.jpg';
 import colorTinterImg from '../assets/projectImages/colortinter.jpg';
+import colorApiImg from '../assets/projectImages/color-api.jpg';
 import mntnImg from '../assets/projectImages/mntn.jpg';
 import alertsjsImg from '../assets/projectImages/alertsjs.jpg';
 import gustavevensson_01 from '../assets/projectImages/gustavevensson_01.jpg';
@@ -93,11 +94,12 @@ onMounted(() => {
 		<div class="centerContainer">
 			<div id="drag-track" data-mouse-down-at="0" data-prev-percentage="0">
 				<project-card data-aos="fade-up" data-aos-delay="0" webPath="https://color-tinter.web.app/" gitPath="https://github.com/gustav-evensson/color-tinter" title="Color Tinter" :imgSrc="colorTinterImg" :tools="['vue', 'scss', 'firebase']"/>
-				<project-card data-aos="fade-up" data-aos-delay="0" webPath="https://geweb.se/" title="GeWeb" :imgSrc="gewebImg" :tools="['Nuxt', 'Vue', 'scss', 'firebase']"/>
-				<project-card data-aos="fade-up" data-aos-delay="100" webPath="https://gustav-evensson.github.io/MNTN/" gitPath="https://github.com/gustav-evensson/MNTN" title="MNTN" :imgSrc="mntnImg" :tools="['github pages', 'scss']"/>
-				<project-card data-aos="fade-up" data-aos-delay="200" webPath="https://aerialshots.se/" gitPath="https://github.com/gustav-evensson/aerialshots" title="AerialShots" :imgSrc="aerialShotsImg" :tools="['firebase']" />
-				<project-card data-aos="fade-up" data-aos-delay="300" webPath="https://www.npmjs.com/package/alerts.js?activeTab=readme" gitPath="https://github.com/gustav-evensson/alerts.js" :imgSrc="alertsjsImg" title="Alerts.js" :tools="['npm']" />
-				<project-card data-aos="fade-up" data-aos-delay="400" webPath="https://gustavevensson-v1.web.app/" gitPath="https://github.com/gustav-evensson/gustavevensson-v1" :imgSrc="gustavevensson_01" title="Portfolio v1" :tools="['vue', 'scss', 'firebase']" />
+				<project-card data-aos="fade-up" data-aos-delay="100" webPath="https://geweb.se/" title="GeWeb" :imgSrc="gewebImg" :tools="['Nuxt', 'Vue', 'scss', 'firebase']"/>
+				<project-card data-aos="fade-up" data-aos-delay="200" webPath="https://gustav-evensson.github.io/MNTN/" gitPath="https://github.com/gustav-evensson/MNTN" title="MNTN" :imgSrc="mntnImg" :tools="['github pages', 'scss']"/>
+				<project-card data-aos="fade-up" data-aos-delay="300" webPath="https://aerialshots.se/" gitPath="https://github.com/gustav-evensson/aerialshots" title="AerialShots" :imgSrc="aerialShotsImg" :tools="['firebase']" />
+				<project-card data-aos="fade-up" data-aos-delay="400" webPath="https://www.npmjs.com/package/alerts.js?activeTab=readme" gitPath="https://github.com/gustav-evensson/alerts.js" :imgSrc="alertsjsImg" title="Alerts.js" :tools="['npm']" />
+				<project-card data-aos="fade-up" data-aos-delay="500" webPath="https://gustavevensson-v1.web.app/" gitPath="https://github.com/gustav-evensson/gustavevensson-v1" :imgSrc="gustavevensson_01" title="Portfolio v1" :tools="['vue', 'scss', 'firebase']" />
+				<project-card data-aos="fade-up" data-aos-delay="500" webPath="https://color-api-ncwh.onrender.com/bases/tints/8B45A1" gitPath="https://github.com/gustav-evensson/color-api/" :imgSrc="colorApiImg" title="Color API" :tools="['Node.js', 'Express.js']" />
 			</div>
 		</div>
 		<div class="columnDisplay">
@@ -108,6 +110,7 @@ onMounted(() => {
 			<div class="showMoreProjects" :class="{ show: state.showMoreProjects }">
 				<project-card webPath="https://www.npmjs.com/package/alerts.js?activeTab=readme" gitPath="https://github.com/gustav-evensson/alerts.js" :imgSrc="alertsjsImg" title="Alerts.js" :tools="['npm']" />
 				<project-card webPath="https://gustavevensson-v1.web.app/" gitPath="https://github.com/gustav-evensson/gustavevensson-v1" :imgSrc="gustavevensson_01" title="Portfolio v1" :tools="['vue', 'scss', 'firebase']" />
+				<project-card webPath="https://color-api-ncwh.onrender.com/bases/tints/8B45A1" gitPath="https://github.com/gustav-evensson/color-api/" :imgSrc="colorApiImg" title="Color API" :tools="['Node.js', 'Express.js']" />
 			</div>
 			<div class="showMoreBtnContainer">
 				<button v-if="!state.showMoreProjects" class="showMoreBtn" @click="state.showMoreProjects = true">Show More</button>
